21906f64aa17d8509e3f0f394b586ccd0d8ea008
gianmarco zodda <zoddagj@gmail.com>
2024-01-24 17:41:36 -0300

    creacion del modelo "Persona"

---
+++ carrito/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿namespace carrito.Models
+{
+    public class Persona
+    {
+
+        public String Username { get; set; }
+
+        public String Pasword { get; set; }
+
+        public String Email { get; set; }   
+
+        public String FechaAlta { get; set; }
+
+    }
+}
