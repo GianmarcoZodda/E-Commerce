6ea4eec4bd221a0052399415f7f812485b5a2168
gianmarco zodda <zoddagj@gmail.com>
2024-01-30 10:45:23 -0300

    Creacion de la clase CarritoContext

---
+++ carrito/Data/CarritoContext.cs
@@ -0,0 +1,40 @@
+﻿using carrito.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace carrito.Data
+{
+    public class CarritoContext : DbContext
+    {
+
+        public CarritoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Carrito> Carritos { get; set; }
+
+        public DbSet<CarritoItem> CarritoItems { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Compra> Compras { get; set; }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+        public DbSet<Persona> Personas { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        public DbSet<StockItem> StockItems { get; set; }
+
+        public DbSet<Sucursal> Sucursales { get; set; }
+
+    }
+
+        
+
+   
+}
